8737cb5e787521e40babb84a4afdf9ee95c0bb7d
VM <example@example.com>
2019-04-08 19:22:33 +0300

    Update
    New Features

---
+++ js/js.js
@@ -0,0 +1,14 @@
+let numFive = 5; alert( ++numFive );
+
+let res = [ ] + false - null + true;
+
+console.log(res);
+
+let y = 1; 
+let x = y = 2; 
+// alert(x);
+
+let res2 = [ ] + 1 + 2;
+
+
+// alert( 2 && 1 && null && 0 && undefined );
