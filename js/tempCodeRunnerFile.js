befa2c9595461ed3548dca6e34d980c971910c75
VM <example@example.com>
2019-04-08 16:16:51 +0300

    1 HW
    Первая Дом. Работа

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+let money = prompt("Сколько зарабатываете", "0");
+// let time = prompt("Дата, образец dd-mm-yyyy"); не используеться при учёте данных
+
+let appData = {
+    budget: money,
+    // timeData: time, не используеться при учёте данных
+    expenses: {}
+};
+
+// let question1 = prompt("Потребность в деньгах на месяц", '0'); не используеться при учёте данных
+// let question2 = prompt("Во сколько обойдется квартира?", '0'); не используеться при учёте данных
+
+// appData.expenses.question1 = question2; не используеться при учёте данных
+
+alert(appData.budget / 31);
